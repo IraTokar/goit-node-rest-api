e1f47ce3b8a04eeb6740a6531b06070abdc889e6
IraTokar <tokar.ira20@gmail.com>
2025-03-23 11:43:18 +0100

    add contacts

---
+++ services/contactsServices.js
@@ -0,0 +1,50 @@
+
+import fs from 'fs/promises';
+import path from 'path';
+import { nanoid } from 'nanoid';
+
+const contactsPath = path.resolve('db', 'contacts.json'); 
+
+export const listContacts = async () => {
+    const data = await fs.readFile(contactsPath);
+    return JSON.parse(data);
+};
+
+export const getContactById = async (id) => {
+    const contacts = await listContacts();
+    return contacts.find(contact => contact.id === id) || null;
+};
+
+export const addContact = async (body) => {
+    const contacts = await listContacts();
+    const newContact = {
+        id: nanoid(),
+        ...body,
+    };
+    contacts.push(newContact);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+    return newContact;
+};
+
+export const removeContact = async (id) => {
+    const contacts = await listContacts();
+    const index = contacts.findIndex(contact => contact.id === id);
+    if (index === -1) {
+        return null;
+    };
+    const [result] = contacts.splice(index, 1);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+    return result;
+};
+
+export const updateContact = async (id, body) => {
+  const contacts = await listContacts();
+  const index = contacts.findIndex(item => item.id === String(id));
+  if (index === -1) {
+    return null;
+  }
+
+  contacts[index] = { ...contacts[index], ...body };
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return contacts[index];
+};
